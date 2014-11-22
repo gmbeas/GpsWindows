eb5aebb1efbec4138ef82a2bc35a4aeb8863093f
gmbeas <gmbeas@gmail.com>
2014-11-22 19:50:46 -0300

    Acualizacion modulo
    se agrego modulo que convierte datos a latitud longitud

--- GpsProject/Gps.Server/Program.cs
+++ GpsProject/Gps.Server/Program.cs
@@ -10,6 +10,29 @@ namespace Gps.Server
     {
         static void Main(string[] args)
         {
+            var server = new TcpServer(5001);
+
+            //var xx = Nmea2DecDeg("07133.7947", "W");
+        }
+
+        public static double Nmea2DecDeg(string NmeaLonLat, string Hemisphere)
+        {
+            int inx = NmeaLonLat.IndexOf(".");
+            if (inx == -1)
+            {
+                return 0;    // Invalid syntax
+            }
+            string minutes_str = NmeaLonLat.Substring(inx - 2);
+            double minutes = Double.Parse(minutes_str, new
+                      System.Globalization.CultureInfo("en-US"));
+            string degrees_str = NmeaLonLat.Substring(0, inx - 2);
+            double degrees = Convert.ToDouble(degrees_str) + minutes / 60.0;
+            if (Hemisphere.Equals("W") || Hemisphere.Equals("S"))
+            {
+                degrees = -degrees;
+
+            }
+            return degrees;
         }
     }
 }
